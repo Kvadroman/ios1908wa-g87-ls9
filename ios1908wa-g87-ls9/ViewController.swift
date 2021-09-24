cf4bb9d3b7aa27ba21e4e730613e17e30e952639
Kvadroman <kvadrostoplerman@gmail.com>
2021-09-24 14:51:06 +0300

    Initial Commit

---
+++ ios1908wa-g87-ls9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios1908wa-g87-ls9
+//
+//  Created by Ивченко Антон on 24.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
